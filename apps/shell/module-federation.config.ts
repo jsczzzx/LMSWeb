1b91037a3d24303180338618b8150a882f20a977
“GodDamnGitHub” <jsczzzx@yahoo.com>
2026-01-28 09:35:14 -0500

    deployment fix 2

--- apps/shell/module-federation.config.ts
+++ apps/shell/module-federation.config.ts
@@ -14,7 +14,7 @@ const config: ModuleFederationConfig = {
    * declare module 'my-external-remote';
    *
    */
-  remotes: ['angular', ['react', 'http://localhost:4202/remoteEntry.js']],
+  remotes: ['angular', 'react'],
 };
 
 export default config;
